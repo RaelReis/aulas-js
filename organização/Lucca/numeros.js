cae653cb9e8b76206373b183becf69a7636ab60c
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-04-24 16:59:13 -0300

    eqweqw
    eqweqwe

--- organização/Lucca/numeros.js
+++ organização/Lucca/numeros.js
@@ -4,4 +4,5 @@ const peso2 = Number('2.1')
 console.log(peso1, peso2)
 console.log(Number.isInteger(peso1)) //ta verificandoi se é inteiro
 console.log(Number.isInteger(peso2)) // ta adcionando o number no peso2
-
+ewq
+ewqeqwewqe
