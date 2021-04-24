2964268a5460a3cced48b5bc573fb86a8b6a51b6
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-04-24 16:57:07 -0300

    dsada
    dasd

---
+++ organização/Lucca/tipagemFraca.js
@@ -0,0 +1,7 @@
+let qualquer = 'rafael brabo' // variavel qualquer como string
+console.log(qualquer)
+console.log(typeof qualquer)  //pedindo para ver qual o tipo da variavel
+
+qualquer = 3.1516   //mesma variavel qualquer so que modificada para ser numero agora
+console.log(qualquer)
+console.log(typeof qualquer)
---
+++ organização/Lucca/variaveisConstantes.js
@@ -0,0 +1,12 @@
+var a = 3 // Variavel
+let b = 4 // Variavel(moderna)
+
+var a = 30
+b = 40
+console.log(a, b)
+
+const c = 50  // const(variavel que não pode ser alterada)
+// c = 5000
+console.log(c)
+
+
