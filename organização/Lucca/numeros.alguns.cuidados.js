53f3a7f804937a72ad44e066d51fb70065945c4b
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-04-24 17:25:44 -0300

    push

---
+++ organização/Lucca/numeros.alguns.cuidados.js
@@ -0,0 +1,5 @@
+console.log(7 / 0)
+console.log("10.2" / 2)
+console.log("show!" * 2)
+console.log(0.1 + 0.7)
+console.log((10.345).toFixed(2)) //para arredondar para 10.352
--- organização/Lucca/numeros.js
+++ organização/Lucca/numeros.js
@@ -1,8 +1,15 @@
-const peso1 = 1.1
-const peso2 = Number('2.1')
+const peso1 = 1.0
+const peso2 = Number('2.0')
 
 console.log(peso1, peso2)
 console.log(Number.isInteger(peso1)) //ta verificandoi se é inteiro
 console.log(Number.isInteger(peso2)) // ta adcionando o number no peso2
-ewq
-ewqeqwewqe
+
+const avaliacao1 = 9.871
+const avaliacao2 = 6.871
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+const media = total / (peso1 + peso2)
+
+console.log(media.toFixed(2)) //para formatar duas casas depois da virgula
+console.log(media.toString(2)) //para formatar a string em binario 
