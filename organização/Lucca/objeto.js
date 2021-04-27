0f22422be429de104a0e9b494a97e489851b53a6
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-04-27 19:08:31 -0300

    Create objeto.js

---
+++ organização/Lucca/objeto.js
@@ -0,0 +1,12 @@
+const objeto1 = {}   ///criando o dicionario
+objeto1.nome = 'Celular Ultra Mega'   ///criando a chave nome e o valor celular ultra mega { nome : 'celular ultra mega'}
+objeto1.preço = 4988.90   ///criando a chave preço e o valor 4.988.90 {preço : 4988.90}
+objeto1['Desconto legal'] = 0.40    /// criando chave com uma estrutura diferente {desconto legal : 0.40} obs: evitar assim
+
+console.log(objeto1)
+
+const objeto2 = {
+    nome: 'camisa polo',
+    preço : 79.90
+}
+console.log(objeto2)
