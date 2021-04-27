79b48e4a3ff7aa7d113127cd69c101abfc45d9ff
Rafael Reis <rafael.reeis03@gmail.com>
2021-04-27 19:08:46 -0300

    Att

---
+++ organização/Rafa/objeto.js
@@ -0,0 +1,14 @@
+const objeto1 = {}
+
+objeto1.nome = 'Rafael'
+objeto1['Sobrenome'] = 'Reis'
+
+console.log(objeto1)
+
+const objeto2 = {
+  nome: 'Rafael',
+  sobrenome: 'Reis',
+  idade: '21',
+}
+
+console.log(objeto2)
