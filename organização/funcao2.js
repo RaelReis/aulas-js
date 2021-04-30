96d7fba8a7458149e0230645dc535e4c6f4f234b
Rafael Reis <rafael.reeis03@gmail.com>
2021-04-30 18:47:51 -0300

    Create funcao2.js

---
+++ organização/funcao2.js
@@ -0,0 +1,18 @@
+// armazenamendo uma funcao em uma variavel
+const imprimirSoma = function(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2,3)
+
+//armazenamendo uma funcao arrow em uma variavel
+const soma = (a,b) => {
+    return a + b
+}
+
+console.log(soma(2,3))
+
+
+//retorno implícito
+const subtracao = (a,b) => a - b
+console.log(subtracao(2,3))
