e4d6a5715b5d80cac49cfab5d724b79291a7a89b
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-04 20:18:31 -0300

    eqweqw

---
+++ organização/Lucca/destructuring2.js
@@ -0,0 +1,5 @@
+const [a] = [10] //criando uma array e ja colocando valor nela
+console.log(a)
+
+const [n1, , n3, , n5 , n6 =0] = [10 ,18 , 9 ,40]   //criando array e ja colocando varios valores nela
+console.log(n1,n3,n5,n6)
---
+++ organização/Lucca/destructuring3.js
@@ -0,0 +1,4 @@
+function rand ({nome = 'rafael', sobrenome = 'reis'})  {
+    nome: 'rafael'
+    sobrenome: 'reis'
+}
