7ea6a63b4ed73fec855151215ea27ff6f454348d
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-04 19:46:05 -0300

    Update destructuring.js

--- organização/Lucca/destructuring.js
+++ organização/Lucca/destructuring.js
@@ -11,3 +11,13 @@ const {nome,idade } = pessoa    //pegar nome e idade DE pessoa
 console.log(nome, idade)     //printar o nome e a idade capturados em cima
 
 const {nome: n, idade : i} = pessoa   //criando uma variavel com nome e idade DE pessoa
+console.log(n,i)
+
+const {sobrenome , bemhumorada = true} = pessoa
+console.log(sobrenome, bemhumorada)
+
+const {endereco: {logradouro , numero , cep } } = pessoa
+console.log(logradouro,numero, cep )
+
+const {conta: {ag , num } } = pessoa
+console.log(ag,num)
