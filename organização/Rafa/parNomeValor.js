753bed8f67ba671e3e4561a7be4ae981ba971efd
Rafael Reis <rafael.reeis03@gmail.com>
2021-05-04 18:59:29 -0300

    att

---
+++ organização/Rafa/parNomeValor.js
@@ -0,0 +1,20 @@
+const saudacao = 'Ola'
+
+function exec(){
+    const saudacao = 'Vrau'
+    return saudacao
+}
+
+const client = {
+    nome: 'Rafael',
+    idade: 22,
+    peso: 70,
+    endereco: {
+        longradouro: 'Rua duiashduha',
+        numero: 112
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(client)
