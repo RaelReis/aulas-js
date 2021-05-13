4126b46b8b1f9f7e4e9ea737dd86e029ab3efd98
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-13 16:39:40 -0300

    Create teste_if.js

---
+++ organização/Lucca/controle/teste_if.js
@@ -0,0 +1,11 @@
+function notao (nota) {
+    if (nota >= 8) {
+        console.log('Voce passou parabens')
+    }
+    else {
+        console.log('Voce foi reprovado!')
+    }
+}
+
+notao(9)
+notao(6)
