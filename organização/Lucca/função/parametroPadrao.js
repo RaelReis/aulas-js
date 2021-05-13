3eb61d2a5a9a8394709babb3c8b2f0a82dea9d9b
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-13 19:36:54 -0300

    Update parametroPadrao.js

--- organização/Lucca/função/parametroPadrao.js
+++ organização/Lucca/função/parametroPadrao.js
@@ -4,4 +4,11 @@ function soma1(a = 1, b = 3 ,c = 1) {
 
 console.log(soma1(3))
 console.log(soma1(2,2,6))
-console.log(soma1(1,1,3,4,6,7)
+
+
+function teste() { //arguments nao precisa de parametros,
+    for (let i in arguments) {
+        console.log(arguments[i])
+    }
+}
+teste('nazare', 'cupece', 2 , 4, 6)
