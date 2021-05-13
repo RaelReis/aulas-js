0058e7d5a30a6cc9121037ed825fd2f48e3e96c0
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-13 18:03:05 -0300

    Create forIn.js

---
+++ organização/Lucca/controle/forIn.js
@@ -0,0 +1,16 @@
+const notas = [7.2, 8.3 ,8.8 ,10 ,2.5]
+
+for (let i in notas) {
+    console.log(notas[i])
+}
+
+const pessoa = {
+    nome: 'ana',
+    sobrenome: 'silva',
+    idade: 29,
+    peso: 64
+}
+
+for(let atributo in pessoa) {
+    console.log(`${atributo} = ${pessoa[atributo]}`)
+}
