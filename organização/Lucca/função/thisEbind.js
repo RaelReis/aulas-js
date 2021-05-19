911b583e58054e4d515e1a62d808eb92d797bee4
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-19 16:11:17 -0300

    Create thisEbind.js

---
+++ organização/Lucca/função/thisEbind.js
@@ -0,0 +1,17 @@
+const pessoa = {
+    saudacao: 'Bom dia!',
+    falar()  {
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar()  //conflito entre paradigmas : funcional e oo
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
+
+
+
+
